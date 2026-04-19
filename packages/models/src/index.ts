126175b316b14505e98c69cbfec46cf45647211f
gmackie <graham.mackie@gmail.com>
2026-04-19 05:12:59 -0700

    docs: annotate @gmacko/models as OODA-adjacent, not a core dep

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,3 +1,8 @@
+// NOTE: @gmacko/models currently holds OODA-adjacent domain types
+// (exploration threads, branches, messages for chat UI). These will be
+// reshaped during OODA migration (@ooda/thread-model). For Phase 6, this
+// package is NOT a dependency of any new gmacko core package.
+
 export * from "./message";
 export * from "./branch";
 export * from "./thread";
